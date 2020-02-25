8e76a90138b62f17525030ee84b3c59f98ede9d7
raj-vora <raj.jv@somaiya.edu>
2020-02-25 23:29:41 +0530

    countries step 3

--- part2/countries/src/List.js
+++ part2/countries/src/List.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Languages = ({languages}) => {
 	return languages.map( (language) => <li key = {language.name}> {language.name} </li> )
+}
+
+const Weather = ({place}) => {
+    const [ weather, setWeather] = useState({});
+    const hook = () => {
+        axios
+            .get('http://api.weatherstack.com/current?access_key=50d5b5c44914440b9beb99109b8a524d&query='+place)
+            .then( (response) => {
+                console.log(response.data.current);
+                setWeather(response.data.current);
+            });
+    } 
+    useEffect(hook , []);
+
+    return (
+        <div>
+            <h3>Weather in {place}</h3>
+            <p><strong>Temperature: </strong> {weather.temperature} Celsius </p>
+            <img src = {weather.weather_icons} alt = {weather.weather_descriptions} />
+            <div><strong>Wind: </strong> {weather.wind_speed} kph Direction {weather.wind_dir}</div>
+        </div>
+    )
 }
 
 const List = ({countries, showButton}) => {
@@ -27,18 +50,20 @@ const List = ({countries, showButton}) => {
       )
     }
     else if(countries.length === 1) {
-      return (
-        <div>
-          <h2>{countries[0].name}</h2>
-          Capital: {countries[0].capital}<br/>
-          Population: {countries[0].population}
-          <h3>Languages spoken</h3>
-          <ul>
-            <Languages languages = {countries[0].languages} />
-          </ul>
-          <img src = {countries[0].flag} alt = {countries[0].name} width = "255" />
-        </div>
-      )
+
+      	return (
+	        <div>
+	          	<h1>{countries[0].name}</h1>
+	          	Capital: {countries[0].capital}<br/>
+	          	Population: {countries[0].population}
+	          	<h2>Languages spoken</h2>
+	          	<ul>
+	            	<Languages languages = {countries[0].languages} />
+	          	</ul>
+	          	<img src = {countries[0].flag} alt = {countries[0].name} width = "255" />
+	          	<Weather place = {countries[0].capital} />
+	        </div>
+      	)
     }
     else {return (<p>No matches found</p>);}
 }
