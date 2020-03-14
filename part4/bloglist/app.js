4bf2cec6329369a96cc68c6e772d14943c4a13c1
raj-vora <raj.jv@somaiya.edu>
2020-03-14 13:34:16 +0530

    blog list, step2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,22 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
