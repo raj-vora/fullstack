4bb5f4cd24e2488dc1cd11bb74fe030cd0d53910
raj-vora <raj.jv@somaiya.edu>
2020-03-14 17:19:02 +0530

    helper functions and unit tests, step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,16 @@
-const dummy = (blogs) => {
+const dummy = blogs => {
     return 1
 }
 
+const totalLikes = blogs => {
+    var sum = 0
+    blogs.forEach(element => {
+        sum = sum + element['likes']
+    });
+    return sum
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
