732ece42e117474d1d80ba3a2278522ed22a3e9c
raj-vora <raj.jv@somaiya.edu>
2020-03-21 20:40:41 +0530

    bloglist expansion, step4

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,13 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const logger = require('../utils/logger')
 
-blogsRouter.get('/', async(request, response) => {
-    const blogs = await Blog.find({})
-    response.json(blogs.map(blog => blog.toJSON()))
-})
-
-blogsRouter.post('/', async(request, response, next) => {
+blogsRouter.post('/', async(request, response) => {
     const body = request.body
 
     if (body.title === undefined && body.url === undefined) {
@@ -24,12 +18,17 @@ blogsRouter.post('/', async(request, response, next) => {
     response.status(201).json(result)
 })
 
-blogsRouter.delete('/:id', async(request, response, next) => {
+blogsRouter.get('/', async(request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs.map(blog => blog.toJSON()))
+})
+
+blogsRouter.delete('/:id', async(request, response) => {
     await Blog.findByIdAndDelete(request.params.id)
     response.status(204).end()
 })
 
-blogsRouter.put('/:id', async(request, response, next) => {
+blogsRouter.put('/:id', async(request, response) => {
     const body = request.body
 
     const blog = {
