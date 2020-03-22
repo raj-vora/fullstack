ab28f53f50303e051e1060eeabc40b8c47dba6ee
raj-vora <raj.jv@somaiya.edu>
2020-03-22 14:57:13 +0530

    bloglist expansion, step6

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,25 +1,35 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.post('/', async(request, response) => {
     const body = request.body
 
+    const userId = '5e762a9a0ebe5b0dfc638b55'
+    const user = await User.findById(userId)
+    console.log(user)
     if (body.title === undefined && body.url === undefined) {
-        response.status(400)
+        response.status(400).json({ 'error': 'title or url missing' })
     }
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes === undefined ? 0 : body.likes
+        likes: body.likes === undefined ? 0 : body.likes,
+        user: userId
     })
 
-    const result = await blog.save()
-    response.status(201).json(result)
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.status(201).json(savedBlog)
 })
 
 blogsRouter.get('/', async(request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({})
+        .populate('user', { username: 1, name: 1 })
+
     response.json(blogs.map(blog => blog.toJSON()))
 })
 
