b1cff6280be0fc6efc238eb7abeca94055e4368b
raj-vora <raj.jv@somaiya.edu>
2020-05-04 21:27:39 +0530

    8.5: Books by genre

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -103,7 +103,10 @@ const typeDefs = gql`
     type Query {
         bookCount: Int!
         authorCount: Int!
-        allBooks(author: String): [Book!]!
+        allBooks(
+            author: String
+            genre: String
+        ): [Book!]!
         allAuthors: [Author!]!
     }
 `
@@ -113,10 +116,13 @@ const resolvers = {
         bookCount: () => books.length,
         authorCount: () => authors.length,
         allBooks: (root, args) => {
-            if(!args.author) {
+            if(!(args.author || args.genre)) {
                 return books
             }
-            return books.filter(book => book.author === args.author)
+            if(args.author){
+                return books.filter(book => book.author === args.author)
+            }
+            return books.filter(book => book.genres.includes(args.genre))
         },
         allAuthors: () => authors
     },
