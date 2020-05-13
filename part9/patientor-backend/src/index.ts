cd89f9b4d319adebd3a191e328fcc68b599332cb
raj-vora <raj.jv@somaiya.edu>
2020-05-13 23:49:33 +0530

    9.10: Patientor backend, step3

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
-const app = express();
 import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
+
+const app = express();
 app.use(express.json());
 app.use(cors());
 const PORT=3001;
@@ -9,6 +11,8 @@ app.get('/api/ping', (_request, response) => {
     console.log('someone pinged here');
     response.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
