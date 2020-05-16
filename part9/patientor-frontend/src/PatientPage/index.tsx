720778a6089d570a7a4a06b7e0085124bbab48c2
raj-vora <raj.jv@somaiya.edu>
2020-05-16 21:45:22 +0530

    9.22: patientor, step7

--- part9/patientor-frontend/src/PatientPage/index.tsx
+++ part9/patientor-frontend/src/PatientPage/index.tsx
@@ -1,10 +1,56 @@
 import React from "react";
 import axios from "axios";
-import { Icon } from "semantic-ui-react";
-import { Patient } from "../types";
+import { Icon, SemanticCOLORS } from "semantic-ui-react";
+import { Patient, Entry, Diagnosis } from "../types";
 import { apiBaseUrl } from "../constants";
 import { useStateValue, singlePatient } from "../state";
 import { useParams } from "react-router-dom";
+
+const EntryDetails: React.FC<{ entry: Entry; diagnoses: Diagnosis[] }> = ({ entry, diagnoses }) => {
+
+    switch (entry.type) {
+        case "Hospital":
+            return(
+                <>
+                    {entry.discharge.criteria}: {entry.discharge.date}
+                </>
+            );
+        case "HealthCheck":
+            let rating: SemanticCOLORS = "green";
+            switch (entry.healthCheckRating) {
+                case 0:
+                    rating = "green";
+                    break;
+                case 1: 
+                    rating = "yellow";
+                    break;
+                case 2:
+                    rating = "orange";
+                    break;
+                case 3:
+                    rating ="red";
+                    break;
+                default:
+                    break;
+            }
+            return (
+                <>
+                    <Icon color={rating} name="heart" />
+                </>
+            );
+        case "OccupationalHealthcare": 
+            return (
+                <>
+                    {entry.employerName}<br />
+                    {entry.sickLeave?.startDate} - {entry.sickLeave?.endDate}
+                </>
+            );
+        default:
+            return (<div>
+
+            </div>);
+    }
+};
 
 const PatientPage: React.FC = () => {
     const { id } = useParams<{ id: string }>();
@@ -23,17 +69,41 @@ const PatientPage: React.FC = () => {
     if(!patient){
         return null;
     }
+
+    let gender = "";
+    switch (patient.gender) {
+        case "male":
+            gender = "man";
+            break;
+        case "female":
+            gender = "woman";
+            break;
+        case "other": 
+            gender = "transgender";
+            break;
+        default:
+            break;
+    }
+    const boxStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5,
+        borderRadius: 10
+    };
     
     return(
         <div>
-            <h1>{patient.name} <Icon className={patient.gender} /> </h1> 
+            <h1>{patient.name} <Icon className={gender} /> </h1> 
             <p>ssn: {patient.ssn}</p>
             <p>Occupation: {patient.occupation}</p>
             <h2>Entries</h2>
             {patient.entries.map(entry => 
-                <div key={entry.id}>
-                    {entry.date}<br />
-                    {entry.description}
+                <div key={entry.id} style={boxStyle}>
+                    <h4>{entry.date}</h4>
+                    {entry.description}<br />
+                    <EntryDetails diagnoses={diagnoses} entry={entry} />
                     <ul>
                         {entry.diagnosisCodes?.map(code =>
                             <li>{code} {diagnoses.find(d => d.code===code)?.name}</li>
